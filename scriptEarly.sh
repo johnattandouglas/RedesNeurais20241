a96e0c5d91d709a64d10da9b0050bdaefb9efbbc
johnattandouglas <johnattandouglas@gmail.com>
2024-06-19 11:00:37 -0300

    Atualização CPU

--- scriptEarly.sh
+++ scriptEarly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=GSEarly
 #SBATCH --ntasks=1
-# SBATCH --cpus-per-task=8
-#SBATCH --mem 32G 
+# SBATCH --cpus-per-task=4
+#SBATCH --mem 16G 
 #SBATCH -c 8 	 # Total de cores que quero utilizar
 #SBATCH --gpus=1 # total de GPUs que vou usar
 #SBATCH -p short # Se vai ser um experimento tipo long (Até 7 dias ) ou  short (até 2 dias)
