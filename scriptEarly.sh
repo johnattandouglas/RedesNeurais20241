be397463923410f11de0b0999ae78fc12b181688
johnattandouglas <johnattandouglas@gmail.com>
2024-06-19 10:52:53 -0300

    Arquivo para rodar o Early Stoppinh

---
+++ scriptEarly.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=GSEarly
+#SBATCH --ntasks=1
+# SBATCH --cpus-per-task=16
+#SBATCH --mem 32G 
+#SBATCH -c 16 	 # Total de cores que quero utilizar
+#SBATCH --gpus=1 # total de GPUs que vou usar
+#SBATCH -p short # Se vai ser um experimento tipo long (Até 7 dias ) ou  short (até 2 dias)
+#SBATCH --mail-type=BEGIN,END,FAIL # quais ocorrências eu quero ser notificado por e-mail
+#SBATCH --mail-user=jdfv@cin.ufpe.br
+# SBATCH --output=job_output.txt
+# SBATCH --error=job_error.txt
+
+# carregar versão python
+module load Python/3.10
+# Ativando ambiente
+source $HOME/lorien/bin/activate
+
+python gridsearchEarly.py
